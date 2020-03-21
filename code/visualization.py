e2075c4208bb8ab0685ec946f183940cd5c754d0
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2020-03-21 18:46:55 -0300

    Adding fuction to plot variance

--- code/visualization.py
+++ code/visualization.py
@@ -1,12 +1,40 @@
 import matplotlib.pyplot as plt
+from pandas import Series
 
-def checking_variance(var):
+def plot_variance_accumulate(var):
 
     fig, ax = plt.subplots(figsize=(12, 5))
 
     ax = var.sort_values().drop('time').plot.bar(ax=ax)
 
     plt.ylabel("Accumulated variance per channel")
+    plt.xlabel("EEG Channel")
+    
+    return fig
+
+
+def plot_variance_by_file(variance_by_file):
+
+    fig, ax = plt.subplots(figsize=(12, 5))
+    
+    var = [file.drop('time').sort_values().index[-1] for file in variance_by_file]
+    
+    ax = Series(var).value_counts().sort_values().plot.bar(ax=ax)
+
+    plt.ylabel("Accumulated rank per channel per file")
+    plt.xlabel("EEG Channel")
+    
+    return fig
+
+def plot_variance_by_pearson(variance_per_person):
+
+    fig, ax = plt.subplots(figsize=(12, 5))
+    
+    var = [file.drop('time').sort_values().index[-1] for file in variance_per_person]
+    
+    ax = Series(var).value_counts().sort_values().plot.bar(ax=ax)
+
+    plt.ylabel("Accumulated rank per channel per pearson")
     plt.xlabel("EEG Channel")
     
     return fig
