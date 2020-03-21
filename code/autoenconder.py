baabff247afacacc297de4be66cc51f8d9bbd24c
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2020-03-21 18:48:24 -0300

    Fixing import problem.

--- code/autoenconder.py
+++ code/autoenconder.py
@@ -5,6 +5,7 @@ Description
 from sklearn.base import BaseEstimator
 from tensorflow.keras import backend as K
 from tensorflow.keras import losses
+from tensorflow.keras.models import Model
 from tensorflow.keras.layers import (
     Input,
     Dense,
